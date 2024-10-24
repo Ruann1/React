f53ce40b142844637a6519579ac06db1aebd1900
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-10-24 14:37:38 -0300

    use state, Eventos e eventos por props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css'
 import Evento from'./components/evento'
 import Form from './components/form'
+import Botao from './components/button'
 
 function App() {
   
-  function soma(a, b){
-    return a + b
-  } 
+  const sum = (a, b) => a + b
   
 
 return (
   <div className='App'>
+    <p>soma: {sum(1,3)}</p>
     <Evento numero="1" />
     <Evento numero="2" />
     <Form />
+    <Botao  />
   </div>
 
 )
---
+++ vite-project/src/components/button.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function contador() {
+    const [conta, setConta] = useState(0);
+    function incremento() {
+         setConta(conta + 1);
+    }
+    return(
+        <div>
+            
+            <p>Voce clicou no {conta} vezes</p>
+            <button onClick={incremento}>Clique Aqui</button>
+
+        </div>
+    )
+}
+
+export default contador
