64c879b7de30ff5eae987dccde7d631a6b74153f
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-11-01 20:33:20 -0300

    React Router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,27 @@
-import './App.css'
-
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  
-  
-  
-
-return (
-  <div className='App'>
-    <h1>State Lift</h1>
-  </div>
-
-)
+  return (
+    
+    <div>
+      
+        <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} /> 
+          <Route path='/empresa' element={<Empresa />}/>
+          <Route path='/contato' element={<Contato />}/>
+        </Routes>
+        </BrowserRouter>
+      
+    </div>
+    
+);
 
 }
 
